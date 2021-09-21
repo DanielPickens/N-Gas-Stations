3bd3f5aa1a3849bb10104803433dc6732f4ea790
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-09-20 22:53:00 -0600

    Create gasstations.js

---
+++ gasstations.js
@@ -0,0 +1,29 @@
+var canCompleteCircuit = function(gas, cost) {
+     var start = 0;
+        var curTank = 0, tank = 0;
+        for(let i = 0; i < gas.length; i++){
+            let curGain = gas[i] - cost[i];
+            tank += curGain;
+            if(curTank + curGain < 0){
+                start = i + 1;
+                curTank = 0;
+            }
+            else
+                curTank += curGain;
+        }
+        if(tank < 0)
+            return -1;
+        else
+            return start;
+    }
+
+
+
+
+
+/*
+Success
+Details 
+Runtime: 68 ms, faster than 94.84% of JavaScript online submissions for Gas Station.
+Memory Usage: 39 MB, less than 67.66% of JavaScript online submissions for Gas Station.
+*/
